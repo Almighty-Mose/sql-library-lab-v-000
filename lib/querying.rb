6442aba1bd46c1da6e69b3061791d43f29f4aaa7
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-09 23:10:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,6 +42,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(book_id)
     FROM characters
-    JOIN character_books ON characters.id = character_books.character_id
+    INNER JOIN character_books ON characters.id = character_books.character_id
     ORDER BY characters.name"
 end
