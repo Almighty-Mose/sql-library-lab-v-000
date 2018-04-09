dbf0bab373db981939ef33c837c99a1259087f39
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-09 22:57:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,8 @@ def select_series_title_with_most_human_characters
     FROM series
     INNER JOIN characters ON series.id = characters.series_id
       WHERE characters.species = 'human'
-      GROUP BY COUNT(*)"
+      GROUP BY characters.species
+      ORDER BY COUNT(*)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
